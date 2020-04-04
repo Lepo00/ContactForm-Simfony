6ef6bf99f6ef1cc6dae18a77de40e79bec23ceb6
Lepo00 <luca.leporati@outlook.com>
2020-04-04 19:18:01 +0200

    creazione editForm

---
+++ src/Form/Edit.php
@@ -0,0 +1,23 @@
+<?php 
+    namespace App\Form;
+    use Symfony\Component\Form\AbstractType;
+    use Symfony\Component\Form\Extension\Core\Type\NumberType;
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
+    use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+    use Symfony\Component\Form\Extension\Core\Type\EmailType;
+    use Symfony\Component\Form\Extension\Core\Type\TelType;
+    use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+    use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+    use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+    use Symfony\Component\Form\FormBuilderInterface;
+
+    
+    class ContattoForm extends AbstractType {     
+
+        public function buildForm(FormBuilderInterface $builder, array $options){
+        $builder
+            ->add('notaOperatore', TextareaType::class,['required' => false,])
+            ->add('save', SubmitType::class);
+        }
+    
+    }
